eceabc1c45de920e0ba984cae991b676d7378f65
Arpit <arpit_jain@live.com>
2018-08-08 21:05:00 +0530

    Home page

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+    $('.header-slider--saree, .header-slider--gown, .header-slider--suit').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            991: {
+                items: 1
+            }
+        }
+    })
+
+    $('.header-mem-plan').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            991: {
+                items: 1
+            }
+        }
+    })
+
+    $('.combo-slider, .trending-slider, .recent-slider').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        autoplay: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            400: {
+                items: 2
+            },
+            750: {
+                items: 3,
+                nav: true
+            },
+            1000: {
+                items: 5,
+                nav: true
+            }
+        }
+    })
+
+    $('.nav-main > ul > li.dropdown').hover(function (event) {
+        event.preventDefault();
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    }, function () {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    });
+    $('.header-menu .btn-group').hover(function (event) {
+        event.preventDefault();
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    }, function () {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    });
+});
